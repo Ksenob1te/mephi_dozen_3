89133e9fb2b1d7cd92b3abbf02e841a126f94ec8
Ksenob1te <Skalini2004>
2023-09-18 22:20:29 +0300

    menu test

--- menu/linux/input_char.cpp
+++ menu/linux/input_char.cpp
@@ -2,20 +2,12 @@
 
 int kbhit(void)
 {
-    struct termios oldt, newt;
     int ch;
     int oldf;
 
-    tcgetattr(0, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(0, TCSANOW, &newt);
     oldf = fcntl(0, F_GETFL, 0);
     fcntl(0, F_SETFL, oldf | O_NONBLOCK);
-
     ch = getchar();
-
-    tcsetattr(0, TCSANOW, &oldt);
     fcntl(0, F_SETFL, oldf);
 
     if(ch != EOF) {
